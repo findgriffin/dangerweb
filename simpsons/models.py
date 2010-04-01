80515c2caa6d71b20cd95999d73696247c3d8dbb
David Griffin <david@serafina.(none)>
2010-04-01 08:58:00 -0600

    proper commit

--- simpsons/models.py
+++ simpsons/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Keyword(models.Model):
+    score = models.IntegerField()
+    word = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return self.word+u" | score:"+unicode(self.score)
+        
+
+class Character(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.firstname+self.lastname
+
+class Quote(models.Model):
+    source = models.CharField(max_length=30)
+    epno = models.SmallIntegerField()
+    title = models.CharField(max_length=40)
+    season = models.SmallIntegerField()
+    characters = models.ManyToManyField(Character)
+    keywords = models.ManyToManyField(Keyword)
+    popularity = models.SmallIntegerField()
+    text = models.TextField()
+
+    def __unicode__(self):
+        return unicode(self.season)+u"."+unicode(self.epno)+self.text
--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
-
+import libquote.py
 def results(request):
+    if 'q' in request.GET
+
     return render_to_response('simpson.html')
 def home(request):
     return render_to_response('simpson.html')
